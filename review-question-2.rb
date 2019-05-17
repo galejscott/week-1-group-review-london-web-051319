535b73b0e56fe338cb223cf585ee28701e071419
Scott <galejscott@gmail.com>
2019-05-17 14:49:23 +0100

    Done. Finally

--- review-question-2.rb
+++ review-question-2.rb
@@ -2,24 +2,41 @@
 
 class Car
 
+    attr_accessor :make, :model
+
+    @@all
+
+    def initialize(make, model)
+        @make = make
+        @model = model
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.drive
+        puts "VVROOOOOOOOOOOOM!"
+    end
 
 
 end
 
-volvo_lightning = Car.new("Volvo", "Lightning")
-yugo = Car.new("Zastava", "Yugo")
-lada = Car.new("AvtoVAZ", "Lada")
+# volvo_lightning = Car.new("Volvo", "Lightning")
+# yugo = Car.new("Zastava", "Yugo")
+# lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-#=> "Volvo"
-volvo_lightning.model
-#=> "Lightning"
+# volvo_lightning.make
+# #=> "Volvo"
+# volvo_lightning.model
+# #=> "Lightning"
 
-Car.drive
-# => "VROOOOOOOOOOOOM!"
+# Car.drive
+# # => "VROOOOOOOOOOOOM!"
 
-Car.all
-#=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
+# Car.all
+# #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
 BONUS:
 
--- review-question-3.rb
+++ review-question-3.rb
@@ -1,5 +1,66 @@
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
+require 'pry'
+class User
+    attr_accessor :name, :photo, :comment
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def photos
+        Photo.all.select {|photo| photo.user == self }
+    end
+
+end
+
+class Photo
+    attr_accessor :user, :photo, :comment
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def comments
+        Comment.all.select {|comment| comment.photo == self }
+    end
+
+    def make_comment(comment)
+        Comment.new(comment, self)
+     end
+
+end
+
+class Comment
+
+    attr_accessor :user, :photo, :comment
+
+    @@all = []
+
+    def initialize(comment, photo)
+        @comment = comment
+        @photo = photo
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
 
 
 sandwich_photo = Photo.new
@@ -10,13 +71,14 @@ sandwich_photo.user.name
 sophie.photos
 # => [#<Photo:0x00007fae2880b370>]
 
-
 sandwich_photo.comments
 # => []
-
 sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
+sandwich_photo.make_comment("Hi, Hi")
+
 sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
 
 Comment.all
 #=> [#<Comment:0x00007fae28043700>]
+binding.pry
